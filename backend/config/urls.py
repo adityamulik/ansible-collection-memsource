ec52489a015e9f20942b6273109868c56769ea3f
Aditya Mulik <aditya.mulik@gmail.com>
2022-07-25 09:43:37 -0400

    added django files

---
+++ backend/config/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('api-auth/', include('rest_framework.urls')),
+    path("o/", include('oauth2_provider.urls', namespace='oauth2_provider')),
+]
